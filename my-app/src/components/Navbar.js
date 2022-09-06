d9b6b68f5ffb49a9872e0c53614f7c2fc2b3f5dd
Rose Maina <rosewairimu27@gmail.com>
2022-09-07 02:44:31 +0300

    Updated the Navbar Liks

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
       <>
         <ul>
             <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjSEo__EXpP_zd581Ds_X9HwwbXg_fRPIo6A&usqp=CAU" alt="logo" /></li>      
-            <li><a class="active" href="home">Home</a></li>
-            <li><a href="about">About</a></li>
-            <li><a href="myfavorites">MyFavorites</a></li>
-            <li><a href="login">Login</a></li>
-            {/* <li><a href="signup">Signup</a></li> */}
+            <li><Link className="nav-link" to="/">Home</Link></li>
+            <li><Link className="nav-link" to="/About">About</Link> </li>                                                             ,
+            {/* <li><a href="myfavorites">MyFavorites</a></li> */}
+            <li><Link className="nav-link" to="/Signup">Signup</Link> </li>
         </ul>
       </>
     );
 };
+
+
 export default Navbar;
