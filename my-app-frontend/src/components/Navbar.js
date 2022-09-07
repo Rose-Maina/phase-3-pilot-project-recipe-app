9f6000e537ef65353cc0f4d56489ec79608b3127
Rose Maina <rosewairimu27@gmail.com>
2022-09-07 22:29:58 +0300

    Updated components

---
+++ my-app-frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+      <>
+      <div className="navbar">
+        <ul>
+            <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjSEo__EXpP_zd581Ds_X9HwwbXg_fRPIo6A&usqp=CAU" alt="logo" /></li>      
+            <li><Link className="nav-link" to="/">Home</Link></li>
+            <li><Link className="nav-link" to="/About">About</Link> </li>
+            <li><Link className="nav-link" to="/Myfavorites">MyFavorites</Link> </li>                                                                                                                       
+            <li><Link className="nav-link" to="/Signup">Signup</Link> </li>
+        </ul>
+      </div>
+      </>
+    );
+};
+
+
+export default Navbar;
